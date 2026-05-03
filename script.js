51dc9990568aaa7891b7ced5c6b9df8b0e05019b
rafar95 <159924019+rafar95@users.noreply.github.com>
2026-05-03 11:53:34 +0200

    New Features Update

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+const cards = document.querySelectorAll(".details-container");
+
+cards.forEach(card => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = -(y - centerY) / 25;
+    const rotateY = (x - centerX) / 25;
+
+    card.style.transform = `
+      rotateX(${rotateX}deg)
+      rotateY(${rotateY}deg)
+      scale(1.03)
+    `;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0) rotateY(0) scale(1)";
+  });
+});
